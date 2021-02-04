8878c9755df2a4aa131c7d23a08bf7f33350c117
MrSullivan <rsullivan@stcadocs.com>
2021-02-04 14:19:12 +0000

    added two variables and print commands that call back the variables from the string.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #write your code below this line
+    print ("I will tell you a story but I need some information from you first")
+    print ("What is the main character called? ")
+    charcterName = input ()
+    print ("What is their job? ")
+    characterJob = input()
+    print ("Here is the story: ")
+    print ("Once upon a time there was " + charcterName + ", who was a " + characterJob + ".")
+    print ("On the way to work, " + charcterName + " reflected on life.")
+    print ("Perhaps " + charcterName + " won't be a " + characterJob + " forever.")
+
+
 
 if __name__ == '__main__':
     main()
